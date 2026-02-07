ad8ae7069270ad43556e251348e2d6ae08d829ea
Kaua Biscotto <kaua.biscotto1405@gmail.com>
2026-02-07 17:37:38 -0300

    Gerador de números com entrada de none

--- src/MegaSena.java
+++ src/MegaSena.java
@@ -1,9 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class MegaSena {
 
     static void main(String[] args) {
         Random generate = new Random();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Olá, " + name);
 
         int i = 0;
         while (i < 6) {
